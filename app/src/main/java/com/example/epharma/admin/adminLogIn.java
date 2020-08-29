abc449a81bfd3884806f59fd66ff448a6cf32332
rajan <rajanbhardwaj1997@gmail.com>
2020-08-29 04:20:04 -0400

    finalchanges

--- app/src/main/java/com/example/epharma/admin/adminLogIn.java
+++ app/src/main/java/com/example/epharma/admin/adminLogIn.java
@@ -5,19 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.epharma.R;
 
 public class adminLogIn extends AppCompatActivity {
-
+EditText email,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_log_in);
+        email=findViewById(R.id.email);
+        pass=findViewById(R.id.pass);
     }
 
     public void insideadmin(View view) {
-        Intent intent=new Intent(getApplicationContext(),addProducts.class);
-        startActivity(intent);
+        if (email.getText().toString().equals("admin@gmail.com") && pass.getText().toString().equals("admin") )
+        {
+            Intent intent = new Intent(getApplicationContext(), addProducts.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"check email and password",Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(getApplicationContext(), adminLogIn.class);
+            startActivity(intent);
+        }
     }
 }
